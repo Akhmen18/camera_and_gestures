9d81f9bd55133ab6c7af46c15fb6ec30c253933d
Akhil Menon <akh180599@gmail.com>
2021-03-17 20:46:57 +0530

    Finalise App

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +9,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  File _image;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: _image == null ? Text('No Image') : Image.file(_image),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _openDialogBox,
+          child: Icon(Icons.add_a_photo),
+          tooltip: 'Open Camera',
+        ),
+      ),
+    );
+  }
+
+  Future openCamera() async {
+    var image = File(await ImagePicker()
+        .getImage(source: ImageSource.camera)
+        .then((pickedFile) => pickedFile.path));
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future openGallery() async {
+    var pic = File(await ImagePicker()
+        .getImage(source: ImageSource.gallery)
+        .then((pickedFile) => pickedFile.path));
+    setState(() {
+      _image = pic;
+    });
+  }
+
+  Future<void> _openDialogBox() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.blue.shade200,
+            shape: StadiumBorder(),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  GestureDetector(
+                    child: Text(
+                      'Take a Picture',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    onTap: openCamera,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      'Chose Image',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    onTap: openGallery,
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
